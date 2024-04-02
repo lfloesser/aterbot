a310db9fc564a07f7c14f36bb8c77b29211839d0
Luca <67869984+lfloesser@users.noreply.github.com>
2024-04-02 09:30:04 +0200

    Update web.ts

--- src/web.ts
+++ src/web.ts
@@ -9,34 +9,3 @@ const server = HTTP.createServer((request, response) => {
 	} as const);
 	response.end("<h3>Copy me, the url above!</h3>");
 });
-
-
-
-export default (): void => {
-	server.listen(PORT, () => console.log("Server for UptimeRobot is ready!"));
-};
-
-exports.handler = async (event, context) => {
- const url = 'https://aterbot-dgdp.onrender.com/';
-
- return new Promise((resolve, reject) => {
-   const req = HTTP.get(url, (res) => {
-     if (res.statusCode === 200) {
-       resolve({
-         statusCode: 200,
-         body: 'Server pinged successfully',
-       });
-     } else {
-       reject(
-         new Error(`Server ping failed with status code: ${res.statusCode}`)
-       );
-     }
-   });
-
-   req.on('error', (error) => {
-     reject(error);
-   });
-
-   req.end();
- });
-};
